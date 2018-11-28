e59a0ecc1f81eb0593bc2058e906f0b0c4d9bd95
Menghan Li <menghanl@google.com>
2018-11-28 13:43:53 -0800

    add backend main

---
+++ src/backend/go.mod
@@ -0,0 +1,23 @@
+module main
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-contrib/static v0.0.0-20180827025113-8cbcdde04781
+	github.com/gin-gonic/gin v1.3.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/menghanl/release-git-bot v0.0.0-20180731171011-312346050750
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go/codec v0.0.0-20181127175209-856da096dbdf // indirect
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
+	golang.org/x/oauth2 v0.0.0-20181128211412-28207608b838
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	golang.org/x/sys v0.0.0-20181128092732-4ed8d59d0b35 // indirect
+	google.golang.org/appengine v1.3.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
---
+++ src/backend/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/github"
+	"github.com/menghanl/release-git-bot/ghclient"
+	"github.com/menghanl/release-git-bot/notes"
+	"golang.org/x/oauth2"
+)
+
+var token = flag.String("token", "", "github token")
+
+var tc *http.Client
+
+func commaStringToSet(s string) map[string]struct{} {
+	ret := make(map[string]struct{})
+	tmp := strings.Split(s, ",")
+	for _, t := range tmp {
+		ret[t] = struct{}{}
+	}
+	return ret
+}
+
+func releaseHandler(c *gin.Context) {
+	if *token == "" {
+		*token = os.Getenv("GITHUB_TOKEN")
+	}
+	if *token == "" {
+		*token = c.DefaultQuery("token", "")
+	}
+	if *token != "" {
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: *token},
+		)
+		tc = oauth2.NewClient(ctx, ts)
+	}
+
+	org := c.DefaultQuery("org", "grpc")
+	repo := c.DefaultQuery("repo", "grpc-go")
+	version := c.DefaultQuery("version", "1.12")
+	thanks := new(bool)
+	*thanks = true
+	client := ghclient.New(tc, org, repo)
+	prs := client.GetMergedPRsForMilestone(version[1:] + " Release")
+
+	var (
+		thanksFilter func(pr *github.Issue) bool
+	)
+	if *thanks {
+		urwelcomeMap := commaStringToSet("")
+		verymuchMap := commaStringToSet("")
+		grpcMembers := client.GetOrgMembers("grpc")
+
+		thanksFilter = func(pr *github.Issue) bool {
+			user := pr.GetUser().GetLogin()
+			_, isGRPCMember := grpcMembers[user]
+			_, isWelcome := urwelcomeMap[user]
+			_, isVerymuch := verymuchMap[user]
+
+			return *thanks && (isVerymuch || (!isGRPCMember && !isWelcome))
+		}
+	}
+
+	// generate notes
+
+	ns := notes.GenerateNotes(org, repo, version, prs, notes.Filters{
+		SpecialThanks: thanksFilter,
+	})
+
+	c.JSON(200, ns)
+}
+
+func main() {
+	flag.Parse()
+
+	r := gin.New()
+	path, _ := filepath.Abs("./build")
+	r.Use(static.Serve("/", static.LocalFile(path, true)))
+	r.LoadHTMLGlob(filepath.Join(path, "index.html"))
+	r.NoRoute(func(c *gin.Context) {
+		path := strings.Split(c.Request.URL.Path, "/")
+		if path[1] != "release" {
+			c.JSON(http.StatusNotFound, gin.H{"msg": "no route", "body": nil})
+		} else {
+			c.HTML(http.StatusOK, "index.html", "")
+		}
+	})
+
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+		api.GET("/release", releaseHandler)
+	}
+
+	// Handle all requests using net/http
+	http.Handle("/", r)
+	r.Run(":3000")
+}
